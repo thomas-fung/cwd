183faf559bbb44794635101ffcb6874ef3cae720
emitanaka <dr.emi.tanaka@gmail.com>
2020-08-18 04:33:51 +0000

    Update file(s) "release/." from "numbats/cwd-private"

---
+++ lectures/assets/sparkline.js
@@ -0,0 +1,6 @@
+<script src="libs/htmlwidgets/htmlwidgets.js"></script>
+<script src="libs/htmlwidgets/sparkline.js"></script>
+<script src="libs/htmlwidgets/htmlwidgets.js"></script>
+<script src="libs/htmlwidgets/lib/jquery.sparkline/jquery.sparkline.min.js"></script>
+<script src="libs/htmlwidgets/lib/jquery/jquery.min.js"></script>
+<link rel="stylesheet" href="libs/htmlwidgets/lib/jquery.sparkline/jquery.sparkline.css">
